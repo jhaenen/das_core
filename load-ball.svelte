96c463a4ba6beb98bb2859508f074e8ec98bfe59
jhaenen <jasper@haenenweb.nl>
2023-01-25 12:41:40 +0100

    Inital commit

---
+++ load-ball.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import BallIcon from "./assets/basketball.svg?component";
+    import ErrorIcon from './assets/error.svg?component';
+    import NoWiFiIcon from './assets/wifi-slash.svg?component';
+
+    export let error = false;
+</script>
+
+<template>
+    <div class="flex flex-col items-center justify-end h-[75px] w-full fixed top-[40%] left-0">
+        {#if !error}
+            <BallIcon class="animate-bounce h-10 w-10"/>
+            <span>Laden...</span>
+        {:else}
+            {#if navigator.onLine}
+                <ErrorIcon class="w-9 h-9 mb-3 fill-slate-600" alt=""/>
+                <span>Er is iets fout gegaan</span>
+            {:else}
+                <NoWiFiIcon class="w-10 h-10 mb-3 fill-slate-600" alt=""/>
+                <span>Geen internet verbinding</span>
+            {/if}
+        {/if}
+    </div>
+</template>
---
+++ nav-button.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import ErrorIcon from './assets/error.svg?component';
+
+    export let label = "NO_LABEL";
+    export let Icon = ErrorIcon;
+    export let color = "#000000";
+</script>
+
+<template>
+    <div class="flex flex-col items-center">
+        <svelte:component this={Icon} class="h-[22.5px] w-[22.5px] mb-0.5 sm:hidden" fill={color}/>
+        <span class="font-bold">{label}</span>
+    </div>
+</template>
